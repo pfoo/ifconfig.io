9c4c0864e91d7d220d1fe9d9b77f89023d340ebd
pfoo <pfoo@csnu.org>
2016-04-08 01:25:49 +0200

    Two new headers support * Added support for Do-Not-Track (DNT) header * Added support for cache-control header

--- main.go
+++ main.go
@@ -115,6 +115,9 @@ func mainHandler(c *gin.Context) {
 	c.Set("via", c.Request.Header.Get("Via"))
 	c.Set("forwarded", c.Request.Header.Get("X-Forwarded-For"))
 	c.Set("country_code", c.Request.Header.Get("CF-IPCountry"))
+	r := strings.NewReplacer("0", "No", "1", "Yes")
+	c.Set("dnt", r.Replace(c.Request.Header.Get("DNT")))
+	c.Set("cache", c.Request.Header.Get("cache-control"))
 
 	ua := strings.Split(c.Request.UserAgent(), "/")
 

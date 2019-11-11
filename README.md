daeef53affe97a532870e0a8f1d6609efce8f256
pfoo <pfoo@unscdf.org>
2019-11-11 14:41:42 +0100

    Multiple changes : * Moved database opening to main() * Use goroutines for HTTP proxy * Load only index.html * PUT request support example * Variables renaming * Proxy mode can be defined using PROXY_TYPE environement variable * Env variables uniformisation : FCGI_PORT, FCGI_HOST, HTTP_PORT, HTTP_HOST

--- README.md
+++ README.md
@@ -20,15 +20,18 @@ This is a slightly modified version of https://github.com/georgyo/ifconfig.io :
 
 A few parameters can be defined using export before launching the binary :
 * export GIN_MODE=debug|release
-* export PORT="8081"
-* export HOST="127.0.0.1"
+* export HTTP_PORT="8080"
+* export HTTP_HOST="127.0.0.1"
+* export FCGI_PORT="4000"
+* export FCGI_HOST="127.0.0.1"
+* export PROXY_TYPE="FCGI|HTTP|BOTH"
 
 Required for ip country and ASN support :
 * Download databases from https://dev.maxmind.com/geoip/geoip2/geolite2/ and place GeoLite2-Country.mmdb and GeoLite2-ASN.mmdb on the same directory than the binary.
 
-#### Running behind an HTTP proxy (usefull if you already have a webserver running on port 80) :
-* Run the go program on 127.0.0.1:8081
-* Use apache mod_proxy_http to proxy requests from apache to http://127.0.0.1:8081/
+#### Running behind an HTTP proxy :
+* Run the go program on 127.0.0.1:8080 with PROXY_TYPE environement variable set to HTTP
+* Use apache mod_proxy_http to proxy requests from apache to http://127.0.0.1:8080/
 * Add followings headers to the proxyfied requests :<br>
 	CF-Connecting-IP : The IP address the client is connecting from. Important or the IP will be wrong.<br>
 	CF-Connecting-PORT : The port the client is connecting from. Important or the PORT will be wrong.<br>
@@ -37,12 +40,13 @@ Required for ip country and ASN support :
 * See example apache.http.conf
 
 #### Running behind an FCGI proxy :
+* Run the go program on 127.0.0.1:4000 with PROXY_TYPE environement variable set to FCGI
 * Use apache mod_proxy_fcgi to proxy requests from apache to fcgi://127.0.0.1:4000/
 * See example apache.fcgi.conf
 
 #### Running behind systemd socket activation (see http://0pointer.de/blog/projects/socket-activation.html)
 * Run : systemd-socket-activate -l 8000 ./ifconfig.pm
-* systemd will listen on port 8000, start the program only when required and forward everything to it
+* systemd will listen on port 8000, start the program only when required and forward everything to it using socket
 * If everything is working, you can create two files in /etc/systemd/system/ in order to automatically launch the service :<br>
 	ifconfig.socket (see example file)<br>
 	ifconfig.service (see example file)<br>
--- main.go
+++ main.go
@@ -10,19 +10,21 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
+	"sync"
 	"github.com/coreos/go-systemd/activation"
 	"github.com/gin-gonic/gin"
 	"github.com/oschwald/maxminddb-golang"
 )
 
-// open geolite country database (download from https://dev.maxmind.com/geoip/geoip2/geolite2/)
-var DBCountry, DBCountryerr = maxminddb.Open("GeoLite2-Country.mmdb")
+// Databases path (download from https://dev.maxmind.com/geoip/geoip2/geolite2/)
+var DBCountryPath = "GeoLite2-Country.mmdb"
+var DBASNPath = "GeoLite2-ASN.mmdb"
 
-// open geolite ASN database (download from https://dev.maxmind.com/geoip/geoip2/geolite2/)
-var DBASN, DBASNerr = maxminddb.Open("GeoLite2-ASN.mmdb")
+// init global database variables for GeoIP
+var DBCountry *maxminddb.Reader
+var DBASN *maxminddb.Reader
 
-// struct for country db
+// struct for Country database
 var RecordCountry struct {
 	Country struct {
 		ISOCode string `maxminddb:"iso_code"` // get country iso code
@@ -33,13 +35,13 @@ var RecordCountry struct {
 
 }
 
-// struct for asn db
+// struct for ASN database
 var RecordASN struct {
 	ASNumber int `maxminddb:"autonomous_system_number"`
 	ASName string `maxminddb:"autonomous_system_organization"`
 }
 
-// Logger is a simple log handler, out puts in the standard of apache access log common.
+// Logger is a simple log handler, outputs in the standard of apache commn access log
 // See http://httpd.apache.org/docs/2.2/logs.html#accesslog
 func Logger() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -83,40 +85,38 @@ func mainHandler(c *gin.Context) {
 		c.Abort()
 	}
 
-	//proxy handling stuff
-
-	// use CF-Connecting-IP header as ip if available (this means app is invoked behind a proxy)
+	// use CF-Connecting-IP header as ip if available (this means app is invoked behind an HTTP proxy)
 	cfIP := net.ParseIP(c.Request.Header.Get("CF-Connecting-IP"))
 	if cfIP != nil {
 		ip.IP = cfIP
 	}
 
-	// use CF-Connecting-PORT header as source port if available (this means app is invoked behind a proxy)
+	// use CF-Connecting-PORT header as source port if available (this means app is invoked behind an HTTP proxy)
 	cfPORT := c.Request.Header.Get("CF-Connecting-PORT")
 	if cfPORTnum, err := strconv.Atoi(cfPORT); err == nil {
 		ip.Port = cfPORTnum
 	}
 
-	// Use CF-Connection header instead of HTTP Connection header if available (this means app is invoked behind a proxy)
+	// Use CF-Connection header instead of HTTP Connection header if available (this means app is invoked behind an HTTP proxy)
 	ConnectionHeader := c.Request.Header.Get("Connection")
 	if cfCONN := c.Request.Header.Get("CF-Connection"); cfCONN != "" {
 		ConnectionHeader = cfCONN
 	}
 
 	// AS number and country name stuff
-	var geoip_country, geoip_asn string
+	var GeoIPCountry, GeoIPASN string
 	err = DBCountry.Lookup(ip.IP, &RecordCountry)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
-	geoip_country = RecordCountry.Country.Names.Name
+	GeoIPCountry = RecordCountry.Country.Names.Name+" ("+RecordCountry.Country.ISOCode+")"
 	err = DBASN.Lookup(ip.IP, &RecordASN)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
-	geoip_asn = RecordASN.ASName+" (AS"+strconv.Itoa(RecordASN.ASNumber)+")"
+	GeoIPASN = RecordASN.ASName+" (AS"+strconv.Itoa(RecordASN.ASNumber)+")"
 
-	// Use CF-Protocol header as protocol if available instead default gathered protocol (this means app is invoked behind a proxy)
+	// Use CF-Protocol header as protocol if available instead default gathered protocol (this means app is invoked behind an HTTP proxy)
 	Protocol := c.Request.Proto
 	if cfProto := c.Request.Header.Get("CF-Protocol"); cfProto != "" {
 		Protocol = cfProto
@@ -136,11 +136,11 @@ func mainHandler(c *gin.Context) {
 	c.Set("via", c.Request.Header.Get("Via"))
 	c.Set("forwarded", c.Request.Header.Get("X-Forwarded-For"))
 	//c.Set("country", c.Request.Header.Get("CF-IPCountry")) //determine country using provided header
-	c.Set("country", geoip_country)
-	r := strings.NewReplacer("0", "No", "1", "Yes")
-	c.Set("dnt", r.Replace(c.Request.Header.Get("DNT")))
+	c.Set("country", GeoIPCountry)
+	DNTReplace := strings.NewReplacer("0", "No", "1", "Yes")
+	c.Set("dnt", DNTReplace.Replace(c.Request.Header.Get("DNT")))
 	c.Set("cache", c.Request.Header.Get("cache-control"))
-	c.Set("asn", geoip_asn)
+	c.Set("asn", GeoIPASN)
 
 	ua := strings.Split(c.Request.UserAgent(), "/")
 
@@ -168,9 +168,9 @@ func mainHandler(c *gin.Context) {
 			c.HTML(200, "index.html", c.Keys)
 		}
 		return
-	case "request":
-		c.JSON(200, c.Request)
-		return
+//	case "request":
+//		c.JSON(200, c.Request)
+//		return
 	case "all":
 		if wantsJSON {
 			c.JSON(200, c.Keys)
@@ -182,70 +182,124 @@ func mainHandler(c *gin.Context) {
 
 	fieldResult, exists := c.Get(fields[0])
 	if !exists {
-		c.String(404, "Not Found")
+		c.String(404, fmt.Sprintln("404 Page Not Found"))
 		return
 	}
 	c.String(200, fmt.Sprintln(fieldResult))
-
 }
 
 func main() {
+	var err error
 
-	if DBCountryerr != nil {
-		log.Fatal(DBCountryerr)
-		os.Exit(1)
+	// open Country database and defer closing to end of main()
+	DBCountry, err = maxminddb.Open(DBCountryPath)
+	if err != nil {
+		log.Fatalf("Fatal: %v. Exiting.", err)
 	}
 	defer DBCountry.Close()
 
-	if DBASNerr != nil {
-		log.Fatal(DBASNerr)
-		os.Exit(1)
+	// open ASN database and defer closing to end of main()
+	DBASN, err = maxminddb.Open(DBASNPath)
+	if err != nil {
+		log.Fatalf("Fatal: %v. Exiting.", err)
 	}
 	defer DBASN.Close()
 
+	// gin HTTP init with Recovery middleware and custom Logger
 	r := gin.New()
 	r.Use(gin.Recovery())
 	r.Use(Logger())
-	r.LoadHTMLGlob("templates/*")
+//	r.LoadHTMLGlob("templates/*")
+	r.LoadHTMLFiles("templates/index.html")
 
+	// GET requests to / or /whatever is handled by mainHandler()
 	r.GET("/:field", mainHandler)
 	r.GET("/", mainHandler)
 
-	// Used for FCGI listener and systemd socket
+	// PUT requests to / or /whatever is handled by mainHandler()
+	//r.PUT("/:field", mainHandler)
+	//r.PUT("/", mainHandler)
+
+	// err chan used for FCGI/HTTP listener goroutines and systemd socket-based activation goroutine
 	errc := make(chan error)
 	go func(errc chan error) {
 		for err := range errc {
-			panic(err)
+			log.Panic(err)
 		}
 	}(errc)
 
-	// Create the FCGI listener
-	fcgiPort := os.Getenv("FCGI_PORT")
-	fcgiHost := os.Getenv("FCGI_HOST")
-	if fcgiPort == "" { fcgiPort = "4000"; }
-	if fcgiHost == "" { fcgiHost = "127.0.0.1"; }
-	fcgi_listen, err := net.Listen("tcp", fcgiHost + ":" + fcgiPort)
-	if err != nil {
-		panic(err)
-	}
-	go func(errc chan error) {
-		errc <- fcgi.Serve(fcgi_listen, r)
-	}(errc)
+	// This will later be set to true if running as systemd socket-based activation, ensuring we are not defaulting to HTTP proxy in this case
+	var UsingSystemd bool = false
 
-	// Create the systemd socket listener (port provided by systemd)
+	// If PROXY_TYPE environement variable is set, ensure it is either FCGI, HTTP or BOTH
+	ProxyType := os.Getenv("PROXY_TYPE")
+	if ProxyType != "" && strings.EqualFold(ProxyType, "FCGI") == false && strings.EqualFold(ProxyType, "HTTP") == false && strings.EqualFold(ProxyType, "BOTH") == false {
+		log.Fatalf("Fatal: PROXY_TYPE environement variable must be either FCGI, HTTP or BOTH. Current is %v. Exiting.", ProxyType)
+	}
+
+	// init wg waitgroup var which will make main() wait until all goroutines (thread) have terminated
+	var wg sync.WaitGroup
+
+	// goroutine for systemd socket-basec activation
+	// Don't scan for ProxyType as it's not necessary and won't be set when running behind systemd socket-based activation
+	// This goroutine won't actually start if not run behind systemd socket-based activation
 	listeners, err := activation.Listeners()
 	if err != nil {
-		fmt.Printf("Could not get systemd listerns with err %q", err)
+		log.Printf("Could not get systemd listeners: %v", err)
 	}
 	for _, listener := range listeners {
+		log.Printf("Starting systemd socket-based activation thread")
+		UsingSystemd = true
+		wg.Add(1) // add 1 goroutine to wait for
+		// systemd socket-based activation goroutine
 		go func(errc chan error) {
+			defer wg.Done() // defer marking the goroutine as done
 			errc <- http.Serve(listener, r)
 		}(errc)
 	}
 
-	// HTTP listener
-	port := os.Getenv("PORT")
-	host := os.Getenv("HOST")
-	if port == "" { port = "8080"; }
-	errc <- r.Run(host + ":" + port)
+	// If PROXY_TYPE environement variable is not set AND we are not using systemd sockets-based activation, we default to HTTP proxy
+	// This allows a default-proxy-mode operation but also prevents defaulting to this proxy type if ran for systemd but systemd goroutine crashed on startup
+	if ProxyType == "" && UsingSystemd == false {
+		log.Printf("PROXY_TYPE environement variable is not set, defaulting to HTTP proxy.")
+		ProxyType = "HTTP"
+	}
+
+	// goroutine for FCGI
+	if strings.EqualFold(ProxyType, "FCGI") || strings.EqualFold(ProxyType, "BOTH") { // strings.EqualFold() allows case insensitive comparison and is more efficient than strings.ToLower=strings.ToLower comparison (https://blog.digitalocean.com/how-to-efficiently-compare-strings-in-go/)
+		fcgiPort := os.Getenv("FCGI_PORT")
+		fcgiHost := os.Getenv("FCGI_HOST")
+		if fcgiPort == "" { fcgiPort = "4000" }
+		if fcgiHost == "" { fcgiHost = "127.0.0.1" }
+		log.Printf("Starting FCGI thread with IP %v and port %v", fcgiHost, fcgiPort)
+		// Create the FCGI listener
+		fcgiListener, err := net.Listen("tcp", fcgiHost + ":" + fcgiPort)
+		if err != nil {
+			log.Panic(err)
+		}
+		defer fcgiListener.Close()
+		wg.Add(1) // add 1 goroutine to wait for
+		// FCGI listener goroutine
+		go func(errc chan error) {
+			defer wg.Done() // defer marking the goroutine as done
+			errc <- fcgi.Serve(fcgiListener, r)
+		}(errc)
+	}
+
+	// goroutine for HTTP
+	if strings.EqualFold(ProxyType, "HTTP") || strings.EqualFold(ProxyType, "BOTH") { // strings.EqualFold() allows case insensitive comparison and is more efficient than strings.ToLower=strings.ToLower comparison (https://blog.digitalocean.com/how-to-efficiently-compare-strings-in-go/)
+		httpPort := os.Getenv("HTTP_PORT")
+		httpHost := os.Getenv("HTTP_HOST")
+		if httpPort == "" { httpPort = "8080" }
+		log.Printf("Starting HTTP thread with IP %v and port %v", httpHost, httpPort)
+		wg.Add(1) // add 1 goroutine to wait for
+		// HTTP listener goroutine
+		go func(errc chan error) {
+			defer wg.Done() // defer marking the goroutine as done
+			errc <- r.Run(httpHost + ":" + httpPort)
+		}(errc)
+	}
+
+	log.Printf("Waiting for connections")
+	wg.Wait() // wait for all goroutine to have terminated
 }
